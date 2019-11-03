cb465d4d94dd169bffe9e1dfd492de65215bda07
Tracy Ogutu <tracyogutu@gmail.com>
2019-11-03 19:39:27 +0300

    add http client

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +16,9 @@ import { SearchFormComponent } from './search-form/search-form.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/http-service.service.ts
+++ src/app/http-service.service.ts
@@ -1,11 +1,38 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Search} from '../app/search';
+import {environment} from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpServiceService {
 
-  constructor() { }
+  searches:Search[]=[];
+  constructor(private http:HttpClient) { }
+
+  searchUser(searchTerm:string){
+    
+    let searchEndpoint= "https://api.giphy.com/v1/gifs/search?api_key="+environment.APIKEY;
+    searchEndpoint += "&q="+searchTerm;
+    let promise =  new Promise((resolve, reject)=>{
+        this.http.get(searchEndpoint).toPromise().then(
+          (results)=>{
+            this.searches=[];
+            for(let i=0; i<results["data"].length; i++){
+              let url = results["data"][i]["images"]["fixed_height"]["url"];
+              let searchresult = new Search(url);
+              this.searches.push(searchresult);
+            }
+            console.log(this.searches);
+            resolve()
+          },
+          (error)=>{
+            console.log(error)
+            reject()
+          }
+        )
+    })
+    return promise;
+  }
 }
